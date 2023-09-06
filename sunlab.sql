a95c84b7f37c72001e63c8af9b8515cbbed2d549
Mark Peschel <msp5393@psu.edu>
2023-09-06 16:55:49 -0400

    Add "user" table; test data.

--- sunlab.sql
+++ sunlab.sql
@@ -1,27 +1,45 @@
 drop table access;
+drop table user;
+
+
+CREATE TABLE user (
+    id VARCHAR(256),
+    kind SET('STUDENT', 'ADMIN'),
+    allowed BOOLEAN,
+    CONSTRAINT PK_user PRIMARY KEY (id)
+);
+
+INSERT INTO user
+    (     kind, allowed,        id)
+VALUES
+    ('STUDENT',       1, 'hsy5393'),
+    ('STUDENT',       1, 'msp3344'),
+    ('STUDENT',       0,     'bob'),
+    (  'ADMIN',       1,  'miccer')
+;
 
 CREATE TABLE access (
-    event_id INT,
-    user_id INT,
+    id INT,
+    user_id VARCHAR(256),
     kind SET('ENTRY', 'EGRESS'),
     timestamp DATETIME,
-    allowed BOOL,
-    CONSTRAINT PK_access PRIMARY KEY (event_id)
+    allowed BOOLEAN,
+    CONSTRAINT PK_access PRIMARY KEY (id)
 );
 
 INSERT INTO access 
-    (event_id, user_id,      kind,             timestamp, allowed)
+    (      id,   user_id,     kind,             timestamp, allowed)
 VALUES
-    (       1,        1,  'ENTRY', '2023-09-03T17:00:00',        1),
-    (       2,        1, 'EGRESS', '2023-09-03T19:00:00',        1),
+    (       1, 'hsy5393',  'ENTRY', '2023-09-03T17:00:00',        1),
+    (       2, 'hsy5393', 'EGRESS', '2023-09-03T19:00:00',        1),
+    
+    (       3, 'hsy5393',  'ENTRY', '2023-09-04T17:00:00',        1),
+    (       4,     'bob',  'ENTRY', '2023-09-04T17:20:00',        0),
+    (       5, 'hsy5393', 'EGRESS', '2023-09-04T19:00:00',        1),
 
-    (       3,        1,  'ENTRY', '2023-09-04T17:00:00',        1),
-    (       4,        2,  'ENTRY', '2023-09-04T17:20:00',        0),
-    (       5,        1, 'EGRESS', '2023-09-04T19:00:00',        1),
-
-    (       6,        1,  'ENTRY', '2023-09-05T19:00:00',        1)
+    (       6, 'hsy5393',  'ENTRY', '2023-09-05T19:00:00',        1)
 ;
+
+select * from user;
 select * from access;
-
-
 commit;
