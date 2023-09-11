9942c51b91a2e99e97539172f725e8651660da5e
Mark Peschel <msp5393@psu.edu>
2023-09-11 14:49:50 -0400

    user.html: Get users from database and show them

--- nginx_local_serve/user.js
+++ nginx_local_serve/user.js
@@ -1,23 +1,47 @@
 (() => {
-    function onButtonUserSearch(event)
+    function showUser(user)
     {
-        event.preventDefault();
+        const tbodyUsers = document.getElementById("tbody-users");
         const templateUserRow = document.getElementById("template-user-row");
         const rowUser = document.importNode(templateUserRow.content.querySelector("tr"), true);
 
-        const dataId = rowUser.querySelector(".data-id");
-        const dataName = rowUser.querySelector(".data-name");
-        const dataKind = rowUser.querySelector(".data-kind");
-        const dataAllowed = rowUser.querySelector(".data-allowed");
+        rowUser.querySelector(".data-id")     .innerText = user.id;
+        rowUser.querySelector(".data-name")   .innerText = user.name;
+        rowUser.querySelector(".data-kind")   .innerText = user.kind;
+        rowUser.querySelector(".data-allowed").innerText = user.allowed;
 
-        dataId.innerText = "972607187";
-        dataName.innerText = "Mark Peschel";
-        dataKind.innerText = "STUDENT";
-        dataAllowed.innerText = "1";
+        tbodyUsers.appendChild(rowUser);
+    }
 
+    function showUsers(users)
+    {
+        // console.log("Showing users", users);
         const tbodyUsers = document.getElementById("tbody-users");
         tbodyUsers.replaceChildren();
-        tbodyUsers.appendChild(rowUser);
+        
+        for (const user of users)
+        {
+            showUser(user);
+        }        
+    }
+
+    function onButtonUserSearch(event)
+    {
+        event.preventDefault();
+        
+        fetch("/user.php")
+        .then(response => response.json())
+        .then(users => showUsers(users));
+        // const users = [
+        //     {
+        //         name: "Mark Peschel",
+        //         id: "972607187",
+        //         kind: "STUDENT",
+        //         allowed: true,
+        //     }
+        // ];
+        // showUsers(users);
+        
     }
 
     function init()
