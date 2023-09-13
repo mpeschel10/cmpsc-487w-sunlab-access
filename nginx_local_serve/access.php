503c798f829c4f22a913132ba7ab9e9552d06ab5
Mark Peschel <msp5393@psu.edu>
2023-09-12 21:56:16 -0400

    access.php fix: Remove debug _GET echo

--- nginx_local_serve/access.php
+++ nginx_local_serve/access.php
@@ -68,7 +68,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET')
     // echo json_encode($result);
     
     $response = array();
-    $response[] = array('userId'=>json_encode($_GET));
     if ($result->num_rows > 0)
     {
         while ($row = $result->fetch_assoc())
