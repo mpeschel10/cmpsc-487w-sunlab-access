9cc56c767c8ef4f04566a95e374e96eb9c6e6441
Mark Peschel <msp5393@psu.edu>
2023-09-12 22:03:41 -0400

    delete_old_records.sh: LEMP image defaults to mysql

--- delete_old_records.sh
+++ delete_old_records.sh
@@ -1,1 +1,3 @@
-mariadb --user=AzureDiamond --password=hunter2 sweng < /opt/sweng/delete_old_records.sql
+#!/bin/sh
+# mariadb --user=AzureDiamond --password=hunter2 sweng < /opt/sweng/delete_old_records.sql
+mysql --user=AzureDiamond --password=hunter2 sweng < /opt/sweng/delete_old_records.sql
